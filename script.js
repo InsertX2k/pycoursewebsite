6763fd1d9f29422112c3bb18883dee5b019ba899
Mr.X <62176660+InsertX2k@users.noreply.github.com>
2021-08-29 16:08:36 +0200

    Uploaded website source code files.
    Don't worry about compiling them, they are automatically compiled by your browser whenever you open them.

---
+++ script.js
@@ -0,0 +1,3 @@
+function printinfo(){
+    alert("That form is not available right now\nBut don't worry, once it is available, you will be able to access it from the same button you summoned this dialogbox\n\nThis course will be available only in 2 languages:\nArabic\nEnglish");
+}
